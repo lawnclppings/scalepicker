d6901135063e48dd7cdc15a3ba42131e8c28221d
lawnclppings <yenversiontwo@gmail.com>
2024-08-17 16:30:34 -0400

    added theming

--- lib/main.dart
+++ lib/main.dart
@@ -5,63 +5,115 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:audioplayers/audioplayers.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    title: 'Scale Picker',
-    home: MyApp(),
-  ));
-}
-
-class Settings {
-  bool a;
-  bool ab;
-  bool b;
-  bool bb;
-  bool c;
-  bool csharp;
-  bool d;
-  bool e;
-  bool eb;
-  bool f;
-  bool fsharp;
-  bool g;
-  bool major;
-  bool minor;
-
-  Settings({
-    required this.a,
-    required this.ab,
-    required this.b,
-    required this.bb,
-    required this.c,
-    required this.csharp,
-    required this.d,
-    required this.e,
-    required this.eb,
-    required this.f,
-    required this.fsharp,
-    required this.g,
-
-    required this.major,
-    required this.minor,
-  });
+  runApp(const MyApp());
 }
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  //var keys = ["A", "A♭", "B", "B♭", "C", "C♯", "D", "E", "E♭", "F", "F♯", "G"];
+  bool darkMode = false;
+  bool isInitialized = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeApp();
+  }
+
+  Future<void> _initializeApp() async {
+    // Load theme preference
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      darkMode = prefs.getBool('darkMode') ?? false;
+      isInitialized = true;
+    });
+  }
+
+  Future<void> _saveThemePreference(bool isDarkMode) async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setBool('darkMode', isDarkMode);
+  }
+
+  void _toggleTheme() async {
+    setState(() {
+      darkMode = !darkMode;
+    });
+    await _saveThemePreference(darkMode); // Save the new theme preference
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Scale Picker',
+      theme: darkMode ? ThemeData.dark(): ThemeData.light(),
+      home: isInitialized
+          ? HomePage(toggleTheme: _toggleTheme)
+          : const Scaffold(
+              body: Center(child: CircularProgressIndicator()),
+            ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  final VoidCallback toggleTheme;
+
+  const HomePage({super.key, required this.toggleTheme});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   bool _a = true, _ab = true, _b = true, _bb = true, _c = true, _csharp = true;
   bool _d = true, _e = true, _eb = true, _f = true, _fsharp = true, _g = true;
   bool major = true, minor = true;
   List<String> keys = [];
   List<String> qualities = [];
-
   bool dronePlaying = false;
-  final player=AudioPlayer();
+  final player = AudioPlayer();
+  final List<String> quotes = [
+    "You can do this!",
+    "Live, laugh, love.",
+    "Your teacher will be proud.",
+    "Good luck!",
+    "Nothing is impossible.",
+    "No, this is Patrick!",
+    "Keep Yourself Safe.",
+    "Intonation...",
+    "Practice 40hrs a day!",
+    "Geniuses are born, not created.",
+    "Practicing scales is like eating your veggies.",
+    "Life without music would B♭.",
+    "Don't give up!",
+    "Slow practice. Or else...",
+  ];
+  String scale = '';
+  String quote = '';
+  String previousQuote = '';
+  String previousScale = '';
+  final Random random = Random();
+
+  @override
+  void initState() {
+    super.initState();
+    initialize();
+  }
+
+  Future<void> initialize() async {
+    await updateKeys();
+    if (keys.isEmpty ||
+        qualities.isEmpty ||
+        (keys.length <= 1 && qualities.length <= 1)) {
+      scale = "C major";
+    }
+    generateScale();
+  }
 
   Future<void> loadSettings() async {
     final prefs = await SharedPreferences.getInstance();
@@ -78,7 +130,6 @@ class _MyAppState extends State<MyApp> {
       _f = prefs.getBool('f') ?? true;
       _fsharp = prefs.getBool('fsharp') ?? true;
       _g = prefs.getBool('g') ?? true;
-
       major = prefs.getBool('major') ?? true;
       minor = prefs.getBool('minor') ?? true;
     });
@@ -108,43 +159,11 @@ class _MyAppState extends State<MyApp> {
       ..addAll([if (major) 'major', if (minor) 'minor']);
   }
 
-  final List<String> quotes = [
-    "You can do this!",
-    "Live, laugh, love.",
-    "Your teacher will be proud.",
-    "Good luck!",
-    "Nothing is impossible.",
-    "No, this is Patrick!",
-    "Keep Yourself Safe.",
-    "Intonation...",
-    "Practice 40hrs a day!",
-    "Geniuses are born, not created.",
-    "Practicing scales is like eating your veggies.",
-    "Life without music would B♭.",
-  ];
-  String scale = '';
-  String quote = '';
-  String previousQuote = '';
-  String previousScale = '';
-  final Random random = Random();
-
-  @override
-  void initState() {
-    super.initState();
-    initialize();
-  }
-
-  Future<void> initialize() async {
-    await updateKeys();
-    if (keys.isEmpty || qualities.isEmpty || (keys.length <= 1 && qualities.length <= 1)) {
-      scale = "C major";
-    }
-    generateScale();
-  }
-
   Future<void> generateScale() async {
-    if (keys.isEmpty || qualities.isEmpty || (keys.length <= 1 && qualities.length <= 1)) {
-      return; //prevent generating literally nothing
+    if (keys.isEmpty ||
+        qualities.isEmpty ||
+        (keys.length <= 1 && qualities.length <= 1)) {
+      return; // Prevent generating literally nothing
     }
     String newScale;
     String randScale;
@@ -152,11 +171,11 @@ class _MyAppState extends State<MyApp> {
       int randomNumber = random.nextInt(keys.length);
       int randomQuality = random.nextInt(qualities.length);
       randScale = "${keys[randomNumber]} ${qualities[randomQuality]}";
-      //convert enharmonics
+      // Convert enharmonics
       if (randScale == "C♯ major") {
         newScale = "D♭ major";
       } else if (randScale == "A♭ minor") {
-         newScale = "G♯ minor";
+        newScale = "G♯ minor";
       } else {
         newScale = randScale;
       }
@@ -165,19 +184,18 @@ class _MyAppState extends State<MyApp> {
     setState(() {
       scale = newScale;
     });
-    //QUOTES
+    // QUOTES
     String newQuote;
     do {
       int randQuote = random.nextInt(quotes.length);
       newQuote = quotes[randQuote];
     } while (newQuote == previousQuote);
-      previousQuote = newQuote;
+    previousQuote = newQuote;
     setState(() {
       quote = newQuote;
     });
 
-
-    //HANDLE AUDIO DRONES
+    // HANDLE AUDIO DRONES
     if (dronePlaying) {
       await play();
     }
@@ -204,24 +222,23 @@ class _MyAppState extends State<MyApp> {
 
     var splitScale = scale.split(' ');
     if (splitScale.isEmpty) {
-      return; // no scale found, do nothing
+      return; // No scale found, do nothing
     }
 
     var audioFile = noteToFile[splitScale[0]];
 
     if (audioFile == null) {
-      return; // if no audio file found, do nothing
+      return; // If no audio file found, do nothing
     }
 
     await player.play(AssetSource(audioFile));
     player.setReleaseMode(ReleaseMode.loop);
-
-    player.setVolume(0.75); // cause the audio files are too loud
+    player.setVolume(0.75); // Cause the audio files are too loud
   }
 
-  void toggleDrone(bool value) {
+  void toggleDrone() {
     setState(() {
-      dronePlaying = value;
+      dronePlaying = !dronePlaying;
     });
     if (dronePlaying) {
       play();
@@ -232,46 +249,39 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          actions: [
-            IconButton(  
-              onPressed: () {
-                // naviate to second page
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const SettingsPage()
-                    ),
-                ).then((_){
-                  setState(() {
-                    updateKeys();
-                  });
-                });
-              },
-              icon: const Icon(Icons.settings),
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            icon: Icon(
+              dronePlaying ? Icons.volume_up : Icons.volume_off,
             ),
-          ],
-          title: Row(
-            children: <Widget>[
-              const Text(
-                'Drone  ',
-                style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-                ), 
-              ),
-              Switch(
-                value: dronePlaying,
-                onChanged: toggleDrone,
-              ),
-            ],
+            onPressed: toggleDrone,
           ),
-        ),
-        body: GestureDetector(
-          onTap: generateScale,
-          child: Center(
+          IconButton(
+            icon: const Icon(Icons.brightness_6),
+            onPressed: widget.toggleTheme,
+          ),
+          IconButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const SettingsPage(),
+                ),
+              ).then((_) {
+                setState(() {
+                  updateKeys();
+                });
+              });
+            },
+            icon: const Icon(Icons.settings),
+          ),
+        ],
+      ),
+      body: Stack( // to make sure the gesturedetector covers the entire screen
+        children: [
+          Center(
             child: Text(
               scale,
               style: const TextStyle(
@@ -280,18 +290,14 @@ class _MyAppState extends State<MyApp> {
               ),
             ),
           ),
-        ),
-        bottomNavigationBar: BottomAppBar(
-            elevation: 0,
-            color: Colors.transparent,
-            child: Text(
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                color: Colors.grey,
-              ),
-              quote,
-            )
+          GestureDetector(
+            onTap: generateScale,
+            child: Container(
+              color: Colors
+                  .transparent, // Make sure it's transparent to not block content
             ),
+          ),
+        ],
       ),
     );
   }
